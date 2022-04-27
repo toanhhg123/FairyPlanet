e3c1dd6373d07e387298fcc8baeeda17fc5442d9
Rol_le <toanhhg123@gmail.com>
2022-04-27 23:27:29 +0700

    fix img

---
+++ js/handleScrollHeader.js
@@ -0,0 +1,27 @@
+export const handleScrollHeader = () => {
+    const btnMenuMB = document.querySelector('.menuMB');
+    const closeMN = document.querySelector('.closeMN');
+    const headerMB = document.querySelector('.headerMB');
+    const header = document.querySelector('header');
+
+    btnMenuMB.onclick = () => {
+        headerMB.style.transform = 'translateY(0)';
+    };
+    closeMN.onclick = () => {
+        headerMB.style.transform = 'translateY(-100%)';
+    };
+
+    window.scroll(0, 0);
+    let scroll = 0;
+    window.onscroll = () => {
+        let scrollTop = window.scrollY;
+        console.log(scrollTop, scroll);
+        if (scrollTop - scroll > 50) {
+            header.style.transform = 'translateY(-100%)';
+            scroll = scrollTop - 50;
+        } else if (scroll - scrollTop > 10) {
+            if (scroll - scrollTop > 30) scroll -= 30;
+            header.style.transform = 'translateY(0%)';
+        }
+    };
+};
