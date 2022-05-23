c0b53421d4fbe9dcdf04e61c34042c4ea0013803
Rol_le <toanhhg123@gmail.com>
2022-05-24 02:19:18 +0700

    123213213

--- js/handleReviewVideo.js
+++ js/handleReviewVideo.js
@@ -1,17 +1,33 @@
+const linkVideo = `<iframe
+src="https://www.youtube.com/embed/ijNnxdW5WWw"
+title="YouTube video player"
+frameborder="0"
+allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+allowfullscreen
+></iframe>
+`;
+
 export const handleVideoReview = () => {
     const reviewVideo = document.querySelector('.review__video');
     const rvModal = document.querySelector('.rv__modal');
+    const video = document.querySelector('.rv__modal div');
+
     const iconClose = document.querySelector('.rv__modal i');
     window.onclick = (e) => {
-        if (e.target === rvModal) rvModal.style.display = 'none';
+        if (e.target === rvModal) {
+            video.innerHTML = ``;
+            rvModal.style.display = 'none';
+        }
     };
 
     iconClose.onclick = () => {
-        console.log(iconClose);
+        video.innerHTML = ``;
+
         rvModal.style.display = 'none';
     };
 
     reviewVideo.onclick = () => {
         rvModal.style.display = 'flex';
+        video.innerHTML = linkVideo;
     };
 };
