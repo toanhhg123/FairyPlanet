e3fb156f5bf23705df4aeee0686f8fb476bc38a9
Rol_le <toanhhg123@gmail.com>
2022-05-28 04:14:52 +0700

    123213

--- js/wpp.js
+++ js/wpp.js
@@ -1,6 +1,5 @@
 const wppWrap = document.querySelector('.wpp__wrap');
 const sections = document.querySelectorAll('section');
-const links = document.querySelectorAll('.nwpp__menu ul li a');
 
 const addActiveLink = (className) => {
     const links = document.querySelectorAll('.nwpp__menu ul li a');
@@ -12,19 +11,12 @@ const addActiveLink = (className) => {
     });
 };
 
-links.forEach((link) => {
-    link.addEventListener('click', () => {
-        links.forEach((link) => link.classList.remove('active'));
-        link.classList.add('active');
-    });
-});
-
 document.addEventListener(
     'scroll',
     (e) => {
         sections.forEach((section) => {
             let id = '';
-            if (pageYOffset > section.offsetTop - screen.height + 100) {
+            if (pageYOffset > section.offsetTop - screen.height + 300) {
                 id = section.getAttribute('id');
             }
             if (id) {
